996bda07b13b57f29495e0e0e85f616ae02a9de3
bboris <boris.bernegger@gmail.com>
2022-10-20 09:20:07 +0200

    Wrap up data exploration by making notebook presentable and removing unnecessary code

--- notebooks/utils/stats.py
+++ notebooks/utils/stats.py
@@ -1,11 +1,12 @@
 import xarray as xr
 import numpy as np
+import xrft
 
 class Stats():
 
-    def __init__(self, ds, num_time_steps=3):
+    def __init__(self, ds, num_samples=3):
         concat_ds = []
-        for i in range(num_time_steps):
+        for i in range(num_samples):
             concat_ds.append(ds[i])
         self.ds = xr.concat(concat_ds, dim='time')
         data_vars = []
@@ -17,6 +18,11 @@ class Stats():
                 data_vars.append(var)
         self.data_vars = data_vars
 
+    def get_label_distribution(self):
+        bg = self.ds[self.labels].where(self.ds[self.labels] == 0).count(dim='time')
+        tc = self.ds[self.labels].where(self.ds[self.labels] == 1).count(dim='time')
+        ar = self.ds[self.labels].where(self.ds[self.labels] == 2).count(dim='time')
+        return bg, tc, ar
 
     def get_mean(self, var):
         return self.ds[var].mean(dim='time')
@@ -30,40 +36,40 @@ class Stats():
     def get_max(self, var):
         return self.ds[var].max(dim='time')
 
-    def get_percentile(self, var, percentile):
-        return self.ds[var].quantile(percentile, dim='time')
+    def get_var(self, var):
+        return self.ds[var].var(dim='time')
+
+    def get_mean_grouped_by_label(self, var):
+        return self.ds[var].groupby(self.ds[self.labels]).mean()
     
     def get_stats(self, var):
         return {
-            'mean': self.get_mean(var),
-            'std': self.get_std(var),
-           'min': self.get_min(var),
-            'max': self.get_max(var),
-            'percentile_10': self.get_percentile(var, 0.1),
-            'percentile_90': self.get_percentile(var, 0.9)
+            'mean': self.get_mean(var).mean().values,
+            'std': self.get_std(var).mean().values,
+            'min': self.get_min(var).min().values,
+            'max': self.get_max(var).max().values,
+            'var': self.get_var(var).mean().values,
+            'mean_grouped_by_label': self.get_mean_grouped_by_label(var).values,
         }
-
-    def get_mean_grouped_by_label(self, var):
-        return self.ds['TMQ'].groupby(self.ds[self.labels]).mean()
 
     def get_corr(self, var1, var2, dim=None):
         if dim is not None:
             return xr.corr(self.ds[var1], self.ds[var2], dim=dim)
-        return xr.corr(self.ds[var1], self.ds[var2])
+        return xr.corr(self.ds[var1], self.ds[var2], dim='time').mean()
+
+    def get_xcorr(self, var1, var2):
+        fft1 = xrft.fft(self.ds[var1], dim=['lat', 'lon'], shift=False, detrend='linear')
+        fft2 = xrft.fft(self.ds[var2], dim=['lat', 'lon'], shift=False, detrend='linear')
+        xcorr = xrft.ifft(fft1 * fft2.conj(), dim=['freq_lat', 'freq_lon'], shift=False, detrend='linear')
+        return xcorr
 
     def get_corr_matrix(self):
-        corr_matrix = np.zeros((len(self.data_vars), len(self.data_vars)))
-        for i, var1 in enumerate(self.data_vars):
-            for j, var2 in enumerate(self.data_vars):
+        return self.get_corr_matrix_vars(self.data_vars)
+
+    def get_corr_matrix_vars(self, vars):
+        corr_matrix = np.zeros((len(vars), len(vars)))
+        for i, var1 in enumerate(vars):
+            for j, var2 in enumerate(vars):
                 corr_matrix[i, j] = self.get_corr(var1, var2)
                 corr_matrix[j, i] = corr_matrix[i, j]
         return corr_matrix
-
-    def get_eigenvalues(self, corr_matrix):
-        return np.linalg.eigvals(corr_matrix)
-
-    def get_variance(self, var):
-        return self.ds[var].var(dim='time')
-
-    def get_num_missing_values(self, var):
-        return self.ds[var].isnull().sum(dim='time')
